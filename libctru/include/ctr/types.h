08afa38c10d1a61cd8eebd8f5921f3071f7f6dba
smea <smealum@gmail.com>
2014-01-30 00:16:45 +0100

    misc : volatile types

--- libctru/include/ctr/types.h
+++ libctru/include/ctr/types.h
@@ -16,6 +16,16 @@
 	typedef signed int s32;
 	typedef signed long long s64;
 
+	typedef volatile u8 vu8;
+	typedef volatile u16 vu16;
+	typedef volatile u32 vu32;
+	typedef volatile u64 vu64;
+
+	typedef volatile s8 vs8;
+	typedef volatile s16 vs16;
+	typedef volatile s32 vs32;
+	typedef volatile s64 vs64;
+
 	typedef u16 wchar;
 
 	typedef u32 Handle;
