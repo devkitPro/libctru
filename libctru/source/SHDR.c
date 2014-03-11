656cb1a9231f60f87aa2c1edf6f2cfde9ee11676
smea <smealum@gmail.com>
2014-03-11 19:00:44 +0100

    SHDR : various fixes

--- libctru/source/SHDR.c
+++ libctru/source/SHDR.c
@@ -15,40 +15,53 @@ DVLB_s* SHDR_ParseSHBIN(u32* shbinData, u32 shbinSize)
 	DVLB_s* ret=malloc(sizeof(DVLB_s));
 	if(!ret)goto exit;
 
-	// //parse DVLB
-	// ret->numDVLE=shbinData[1];
-	// ret->DVLE=malloc(sizeof(DVLE_s)*ret->numDVLE);
-	// if(!ret->DVLE)goto clean1;
+	//parse DVLB
+	ret->numDVLE=shbinData[1];
+	ret->DVLE=malloc(sizeof(DVLE_s)*ret->numDVLE);
+	if(!ret->DVLE)goto clean1;
 
-	// //parse DVLP
-	// u32* dvlpData=&shbinData[2+ret->numDVLE];
-	// ret->DVLP.codeSize=dvlpData[3];
-	// ret->DVLP.codeData=&dvlpData[dvlpData[2]/4];
-	// ret->DVLP.opdescSize=dvlpData[5];
-	// ret->DVLP.opcdescData=&dvlpData[dvlpData[4]/4];
+	//parse DVLP
+	u32* dvlpData=&shbinData[2+ret->numDVLE];
+	ret->DVLP.codeSize=dvlpData[3];
+	ret->DVLP.codeData=&dvlpData[dvlpData[2]/4];
+	ret->DVLP.opdescSize=dvlpData[5];
+	ret->DVLP.opcdescData=&dvlpData[dvlpData[4]/4];
 
-	// //parse DVLE
-	// int i;
-	// for(i=0;i<ret->numDVLE;i++)
-	// {
-	// 	DVLE_s* dvle=&ret->DVLE[i];
-	// 	u32* dvleData=&shbinData[shbinData[2+i]/4];
+	//parse DVLE
+	int i;
+	for(i=0;i<ret->numDVLE;i++)
+	{
+		DVLE_s* dvle=&ret->DVLE[i];
+		u32* dvleData=&shbinData[shbinData[2+i]/4];
 
-	// 	dvle->type=(dvleData[1]>>16)&0xFF;
-	// 	dvle->mainOffset=dvleData[2];
-	// 	dvle->endmainOffset=dvleData[3];
+		dvle->type=(dvleData[1]>>16)&0xFF;
+		dvle->mainOffset=dvleData[2];
+		dvle->endmainOffset=dvleData[3];
 
-	// 	dvle->constTableSize=dvleData[7];
-	// 	dvle->constTableData=(DVLE_constEntry_s*)&dvleData[dvleData[6]/4];
+		dvle->constTableSize=dvleData[7];
+		dvle->constTableData=(DVLE_constEntry_s*)&dvleData[dvleData[6]/4];
 
-	// 	dvle->outTableSize=dvleData[11];
-	// 	dvle->outTableData=(DVLE_outEntry_s*)&dvleData[dvleData[10]/4];
-	// }
+		dvle->outTableSize=dvleData[11];
+		dvle->outTableData=(DVLE_outEntry_s*)&dvleData[dvleData[10]/4];
+	}
 
+	goto exit;
 	clean1:
 		free(ret);
 	exit:
 		return ret;
+}
+
+//hm
+static inline minu8(u8 a, u8 b)
+{
+	if(a<b)return a;
+	return b;
+}
+static inline maxu8(u8 a, u8 b)
+{
+	if(a<b)return b;
+	return a;
 }
 
 void DVLP_SendCode(DVLP_s* dvlp)
@@ -58,7 +71,8 @@ void DVLP_SendCode(DVLP_s* dvlp)
 	GPUCMD_AddSingleParam(0x000F02CB, 0x00000000);
 
 	int i;
-	for(i=0;i<dvlp->codeSize;i+=0x80)GPUCMD_Add(0x000F02CC, &dvlp->codeData[i], ((dvlp->codeSize-i)<0x80)?(dvlp->codeSize-i):0x80);
+	// for(i=0;i<dvlp->codeSize;i+=0x80)GPUCMD_Add(0x000F02CC, &dvlp->codeData[i], ((dvlp->codeSize-i)<0x80)?(dvlp->codeSize-i):0x80);
+	for(i=0;i<dvlp->codeSize;i+=0x80)GPUCMD_Add(0x000F02CC, &dvlp->codeData[i], maxu8(minu8(dvlp->codeSize-i,0x80),0x37)); //not sure why, but anything smaller than 0x37 seems to break stuff atm...
 
 	GPUCMD_AddSingleParam(0x000F02BF, 0x00000001);
 }
@@ -69,17 +83,24 @@ void DVLP_SendOpDesc(DVLP_s* dvlp)
 
 	GPUCMD_AddSingleParam(0x000F02D5, 0x00000000);
 
-	GPUCMD_Add(0x000F02D6, dvlp->opcdescData, dvlp->opdescSize);
+	u32 param[0x20];
+
+	int i;
+	//TODO : should probably preprocess this
+	for(i=0;i<dvlp->opdescSize;i++)param[i]=dvlp->opcdescData[i*2];
+
+	GPUCMD_Add(0x000F02D6, param, dvlp->opdescSize);
 }
 
 void DVLE_SendOutmap(DVLE_s* dvle)
 {
 	if(!dvle)return;
 
-	u32 param[0x8]={0x1F1F1F1F,0x1F1F1F1F,0x1F1F1F1F,0x1F1F1F1F,
-					0x1F1F1F1F,0x1F1F1F1F,0x1F1F1F1F,0x1F1F1F1F};
+	u32 param[0x7]={0x1F1F1F1F,0x1F1F1F1F,0x1F1F1F1F,0x1F1F1F1F,
+					0x1F1F1F1F,0x1F1F1F1F,0x1F1F1F1F};
 
 	int i;
+	//TODO : should probably preprocess this
 	for(i=0;i<dvle->outTableSize;i++)
 	{
 		u32* out=&param[dvle->outTableData[i].regID];
