5045c830a8b6aa7b1b3dcc3f956ddb0a433d536b
plutoo <plutoo@univor.se>
2014-08-21 20:35:12 +0200

    os: Added DSP memory to virt->phys translation.

--- libctru/source/os.c
+++ libctru/source/os.c
@@ -6,10 +6,12 @@ u32 osConvertVirtToPhys(u32 vaddr)
 {
     if(vaddr >= 0x14000000 && vaddr < 0x1c000000)
         return vaddr + 0x0c000000; // LINEAR heap
-    if(vaddr >= 0x30000000 && vaddr < 0x40000000)
-        return vaddr - 0x10000000; // Only available under FIRM v8+ for certain processes.
     if(vaddr >= 0x1F000000 && vaddr < 0x1F600000)
         return vaddr - 0x07000000; // VRAM
+    if(vaddr >= 0x1FF00000 && vaddr < 0x1FF80000)
+        return vaddr + 0x00000000; // DSP memory
+    if(vaddr >= 0x30000000 && vaddr < 0x40000000)
+        return vaddr - 0x10000000; // Only available under FIRM v8+ for certain processes.
     return 0;
 }
 
