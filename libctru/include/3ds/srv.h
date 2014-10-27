337eb60baed9d546027f3ef618cb0f1934ca175d
idunoe <idunoe@asia.com>
2014-10-27 13:17:07 +0800

    SRV: Added <7.X srv:pm

--- libctru/include/3ds/srv.h
+++ libctru/include/3ds/srv.h
@@ -4,3 +4,7 @@ Result srvInit();
 Result srvExit();
 Result srvRegisterClient();
 Result srvGetServiceHandle(Handle* out, char* name);
+
+Result srvPmInit();
+Result srvRegisterProcess(u32 procid, u32 count, void *serviceaccesscontrol);
+Result srvUnregisterProcess(u32 procid);
