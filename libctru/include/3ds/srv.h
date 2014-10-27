d9c3b85d348f4352f3b9808a502032348c850c98
idunoe <idunoe@asia.com>
2014-10-27 13:19:24 +0800

    SRV: misc fix

--- libctru/include/3ds/srv.h
+++ libctru/include/3ds/srv.h
@@ -3,7 +3,7 @@
 Result srvInit();
 Result srvExit();
 Result srvRegisterClient();
-Result srvGetServiceHandle(Handle* out, char* name);
+Result srvGetServiceHandle(Handle* out, const char* name);
 
 Result srvPmInit();
 Result srvRegisterProcess(u32 procid, u32 count, void *serviceaccesscontrol);
