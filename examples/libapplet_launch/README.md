866cb1ff56d67fc565b177f2f8f05cc95dbca073
yellows8 <yellows8@users.noreply.github.com>
2014-11-26 22:12:20 -0500

    Added library applet launching example, this isn't usable from the homebrew launcher.

---
+++ examples/libapplet_launch/README.md
@@ -0,0 +1,7 @@
+libapplet_launch
+=======
+
+Example for launching library applets. This launches the extrapad library applet(Circle Pad Pro calibration applet) when the B button is pressed.
+
+This is not usable from the homebrew launcher.
+
---
+++ examples/libapplet_launch/source/main.c
@@ -0,0 +1,50 @@
+#include <3ds.h>
+
+int main()
+{
+	u32 val, i;
+
+	// Initialize services
+	srvInit();
+	aptInit();
+	hidInit(NULL);
+	gfxInit();
+	//gfxSet3D(true); // uncomment if using stereoscopic 3D
+
+	val = 0x22447899;
+
+	// Main loop
+	while (aptMainLoop())
+	{
+		gspWaitForVBlank();
+		hidScanInput();
+
+		// Your code goes here
+
+		u32 kDown = hidKeysDown();
+		if (kDown & KEY_START)
+			break; // break in order to return to hbmenu
+
+		if (kDown & KEY_B)APT_LaunchLibraryApplet(0x408, 0, NULL, 0);//Launch the extrapad library applet when button B is pressed.
+
+		// Example rendering code that displays a white pixel
+		// Please note that the 3DS screens are sideways (thus 240x400 and 240x320)
+		u32* fb = (u32*)gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
+
+		for(i=0; i<(0x46500>>2); i++)fb[i] = val;
+
+		val+= 0x44;
+
+		// Flush and swap framebuffers
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+	}
+
+	// Exit services
+	gfxExit();
+	hidExit();
+	aptExit();
+	srvExit();
+	return 0;
+}
+
