8536e1e6ef4e49c62acf96510aec25c362db7b1c
mtheall <pigman46@gmail.com>
2014-12-08 12:12:34 -0600

    Update poll.h

--- libctru/include/poll.h
+++ libctru/include/poll.h
@@ -2,17 +2,12 @@
 
 #include <3ds/types.h>
 
-/* only POLLIN confirmed to work so far */
-#define POLLIN		0x001
-#define POLLPRI		0x002
-#define POLLOUT		0x004
-#define POLLERR		0x008
-#define POLLHUP		0x010
-#define POLLNVAL	0x020
-#define POLLRDNORM	0x040
-#define POLLRDBAND	0x080
-#define POLLWRNORM	0x100
-#define POLLWRBAND	0x200
+#define POLLIN		0x01
+#define POLLPRI		0x02
+#define POLLOUT		0x10
+#define POLLERR		0x00 // unknown ???
+#define POLLHUP		0x00 // unknown ???
+#define POLLNVAL	0x20
 
 typedef u32 nfds_t;
 
