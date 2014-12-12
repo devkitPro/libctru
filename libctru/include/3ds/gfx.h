493d7dc55a1ec24f3ade6f47eaad77bcae8907d0
Dave Murphy <davem@devkitpro.org>
2014-12-12 22:57:24 +0000

    add macro to convert 8bit RGB to RGB565

--- libctru/include/3ds/gfx.h
+++ libctru/include/3ds/gfx.h
@@ -3,6 +3,7 @@
 #include <3ds/services/gsp.h>
 
 #define RGB565(r,g,b)  (((b)&0x1f)|(((g)&0x3f)<<5)|(((r)&0x1f)<<11))
+#define RGB8_to_565(r,g,b)  (((b)>>3)&0x1f)|((((g)>>2)&0x3f)<<5)|((((r)>>3)&0x1f)<<11)
 
 typedef enum
 {
