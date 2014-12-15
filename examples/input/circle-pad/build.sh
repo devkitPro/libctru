adeffd27ea8af07d06c3e9f58839e4ae4faad16b
Aurelio Mannara <aurelio.mannara@gmail.com>
2014-12-15 09:59:44 +0100

    Fixed a little mess I made with dir-names :D

--- examples/input/circle-pad/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export DEVKITARM=/Users/aurelio/Programmazione/NintendoDS/devkitARM
-export DEVKITPRO=/Users/aurelio/Programmazione/NintendoDS
-export CTRULIB=/Users/aurelio/Programmazione/NintendoDS/ctrulib/libctru
-make clean
-make
-sh copy.sh
