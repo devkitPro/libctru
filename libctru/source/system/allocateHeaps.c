766ee2c70317ea2e20c0a1bea8484fdb55d2051c
Dave Murphy <davem@devkitpro.org>
2014-12-25 10:50:38 +0000

    split up sysyem initialisation and allow overriding

---
+++ libctru/source/system/allocateHeaps.c
@@ -0,0 +1,24 @@
+#include <3ds/types.h>
+#include <3ds/svc.h>
+
+extern char* fake_heap_start;
+extern char* fake_heap_end;
+u32 __linear_heap;
+u32 __heapBase;
+extern u32 __heap_size, __linear_heap_size;
+
+
+void __attribute__((weak)) __allocateHeaps() {
+	u32 tmp=0;
+
+	// Allocate the application heap
+	__heapBase = 0x08000000;
+	svcControlMemory(&tmp, __heapBase, 0x0, __heap_size, MEMOP_ALLOC, 0x3);
+
+	// Allocate the linear heap
+	svcControlMemory(&__linear_heap, 0x0, 0x0, __linear_heap_size, MEMOP_ALLOC_LINEAR, 0x3);
+	// Set up newlib heap
+	fake_heap_start = (char*)__heapBase;
+	fake_heap_end = fake_heap_start + __heap_size;
+
+}
---
+++ libctru/source/system/initSystem.c
@@ -0,0 +1,36 @@
+#include <sys/iosupport.h>
+#include <string.h>
+#include <3ds/types.h>
+#include <3ds/svc.h>
+
+void (*__system_retAddr)(void);
+
+// Data from _prm structure
+extern void* __service_ptr; // used to detect if we're run from a homebrew launcher
+
+void __system_allocateHeaps();
+void __system_initArgv();
+
+// newlib definitions we need
+void __libc_init_array(void);
+
+
+void __ctru_exit(int rc);
+
+void __attribute__((weak)) initSystem(void (*retAddr)(void))
+{
+
+	// Register newlib exit() syscall
+	__syscalls.exit = __ctru_exit;
+	__system_retAddr = __service_ptr ? retAddr : NULL;
+
+	__system_allocateHeaps();
+
+	// Build argc/argv if present
+	__system_initArgv();
+
+	// TODO: APT init goes here
+
+	// Run the global constructors
+	__libc_init_array();
+}
