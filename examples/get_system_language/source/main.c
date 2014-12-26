9ab796b058d1518fab8a48aee70f5b57201e8ecb
profi200 <fd3194@gmx.de>
2014-12-26 02:57:49 +0100

    Forgot to finish the changes on the get_system_language example.

--- examples/get_system_language/source/main.c
+++ examples/get_system_language/source/main.c
@@ -28,7 +28,8 @@ int main(int argc, char** argv)
 		hidScanInput();
 
 		// Print return value and language code
-		printf("\x1b[0;0HLanguage code: %d", (int)language);
+		printf("\x1b[0;0Hresult: %d", (int)res);
+		printf("\x1b[1;0HLanguage code: %d", (int)language);
 
 		u32 kDown = hidKeysDown();
 		if (kDown & KEY_START)
