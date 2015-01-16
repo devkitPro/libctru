1e61718a303c59ce519a7ce4895d7f0e30b7c1e8
profi200 <fd3194@gmx.de>
2015-01-16 22:44:54 +0100

    And more...

--- libctru/source/srv.c
+++ libctru/source/srv.c
@@ -9,15 +9,15 @@
 
 
 /*
-	The homebrew loader can choose to supply a list of service handles that have
-	been "stolen" from other processes that have been compromised. This allows us
-	to access services that are normally restricted from the current process.
+ The homebrew loader can choose to supply a list of service handles that have
+ been "stolen" from other processes that have been compromised. This allows us
+ to access services that are normally restricted from the current process.
 
-	For every service requested by the application, we shall first check if the
-	list given to us contains the requested service and if so use it. If we don't
-	find the service in that list, we ask the service manager and hope for the
-	best.
- */
+ For every service requested by the application, we shall first check if the
+ list given to us contains the requested service and if so use it. If we don't
+ find the service in that list, we ask the service manager and hope for the
+ best.
+*/
 
 typedef struct {
 	u32 num;
@@ -116,7 +116,7 @@ Result srvGetServiceHandle(Handle* out, const char* name)
 	Result rc = 0;
 
 	/* Look in service-list given to us by loader. If we find find a match,
-     we return it. */
+	   we return it. */
 	Handle h = __get_handle_from_list(name);
 
 	if(h != 0) {
