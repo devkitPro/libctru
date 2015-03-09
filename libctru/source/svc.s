a1c1b44dfde96af8aad9fca8f9336a835f166083
patois <dennis@backtrace.de>
2015-03-09 04:54:03 +0100

    add stubs for svc 0x61, 0x63, 0x64

--- libctru/source/svc.s
+++ libctru/source/svc.s
@@ -319,6 +319,24 @@ svcDebugActiveProcess:
 	str r1, [r2]
 	bx lr
 
+.global svcBreakDebugProcess
+.type svcBreakDebugProcess, %function
+svcBreakDebugProcess:
+	svc 0x61
+	bx lr
+
+.global svcGetProcessDebugEvent
+.type svcGetProcessDebugEvent, %function
+svcGetProcessDebugEvent:
+	svc 0x63
+	bx lr
+	
+.global svcContinueDebugEvent
+.type svcContinueDebugEvent, %function
+svcContinueDebugEvent:
+	svc 0x64
+	bx lr
+
 .global svcGetProcessList
 .type svcGetProcessList, %function
 svcGetProcessList:
