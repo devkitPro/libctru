222bc24165576899786f6dff5d54ce09c1882c4c
Lectem <lectem@gmail.com>
2015-03-21 15:23:17 +0100

    forgot to remove a condition

--- exportdoc.sh
+++ exportdoc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ "$CC" = "clang" ] && [ "$TRAVIS_REPO_SLUG" = "Lectem/ctrulib" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" = "Lectem/ctrulib" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
 sudo apt-get install -qq doxygen
 git clone --branch=gh-pages --single-branch --depth 1 https://${GH_TOKEN}@github.com/Lectem/ctrulib docs
 doxygen Doxyfile
