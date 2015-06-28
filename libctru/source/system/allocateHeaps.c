c62d54d831ec54e2c1a1f86b103ef6d37f4bbbc2
Lectem <lectem@gmail.com>
2015-06-28 13:10:38 +0200

    fix arning types not matching

--- libctru/source/system/allocateHeaps.c
+++ libctru/source/system/allocateHeaps.c
@@ -13,10 +13,10 @@ void __attribute__((weak)) __system_allocateHeaps() {
 
 	// Allocate the application heap
 	__heapBase = 0x08000000;
-	svcControlMemory(&tmp, __heapBase, 0x0, __heap_size, MEMOP_ALLOC, 0x3);
+	svcControlMemory(&tmp, __heapBase, 0x0, __heap_size, MEMOP_ALLOC, MEMPERM_READ | MEMPERM_WRITE);
 
 	// Allocate the linear heap
-	svcControlMemory(&__linear_heap, 0x0, 0x0, __linear_heap_size, MEMOP_ALLOC_LINEAR, 0x3);
+	svcControlMemory(&__linear_heap, 0x0, 0x0, __linear_heap_size, MEMOP_ALLOC_LINEAR, MEMPERM_READ | MEMPERM_WRITE);
 	// Set up newlib heap
 	fake_heap_start = (char*)__heapBase;
 	fake_heap_end = fake_heap_start + __heap_size;
