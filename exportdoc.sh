0c2a52a4678bdfb2d1188de0631fbb1aa2a0af16
Lectem <lectem@gmail.com>
2015-07-12 20:21:05 +0200

    travis CI

---
+++ exportdoc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ "$TRAVIS_REPO_SLUG" = "smealum/ctrulib" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "great-refactor" ]; then
+git clone --branch=gh-pages --single-branch --depth 1 https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG docs
+git rm -rf docs/*
+cd libctru
+doxygen Doxyfile
+mv ./docs/html/* ../docs
+cd ../docs
+git add --all
+git commit -m"Doc generated from commit $TRAVIS_COMMIT"
+git push -f origin gh-pages
+
+fi
