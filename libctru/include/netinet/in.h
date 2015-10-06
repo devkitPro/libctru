1a24e0e0546f385302c03554c7819de8beb40d31
Lectem <lectem@gmail.com>
2015-10-06 11:16:29 -0400

    fixed some netinet/in.h formating

--- libctru/include/netinet/in.h
+++ libctru/include/netinet/in.h
@@ -5,9 +5,9 @@
 
 #define INADDR_ANY       0x00000000
 #define INADDR_BROADCAST 0xFFFFFFFF
-#define INADDR_NONE		 0xFFFFFFFF
+#define INADDR_NONE      0xFFFFFFFF
 
-#define INET_ADDRSTRLEN		16
+#define INET_ADDRSTRLEN  16
 
 /*
  * Protocols (See RFC 1700 and the IANA)
@@ -16,25 +16,25 @@
 #define IPPROTO_UDP        17               /* user datagram protocol */
 #define IPPROTO_TCP         6               /* tcp */
 
-#define	IP_TOS              7
-#define	IP_TTL              8
+#define IP_TOS              7
+#define IP_TTL              8
 #define IP_MULTICAST_LOOP   9
-#define	IP_MULTICAST_TTL   10
-#define	IP_ADD_MEMBERSHIP  11
-#define	IP_DROP_MEMBERSHIP 12
+#define IP_MULTICAST_TTL   10
+#define IP_ADD_MEMBERSHIP  11
+#define IP_DROP_MEMBERSHIP 12
 
 typedef uint16_t in_port_t;
 typedef uint32_t in_addr_t;
 
 struct in_addr {
-	in_addr_t	s_addr;
+	in_addr_t       s_addr;
 };
 
 struct sockaddr_in {
-	sa_family_t	sin_family;
-	in_port_t	sin_port;
-	struct in_addr	sin_addr;
-	unsigned char	sin_zero[8];
+	sa_family_t     sin_family;
+	in_port_t       sin_port;
+	struct in_addr  sin_addr;
+	unsigned char   sin_zero[8];
 };
 
 /* Request struct for multicast socket ops */
