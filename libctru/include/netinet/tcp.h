70e7fb35bc1c2572fa265536fea61c03aee7a963
Lectem <lectem@gmail.com>
2015-10-06 12:48:25 -0400

    fix SOL_SOCKET level options

--- libctru/include/netinet/tcp.h
+++ libctru/include/netinet/tcp.h
@@ -1,5 +1,9 @@
 #pragma once
 
-//#define SOL_TCP	???
+#define SOL_TCP     6    /* TCP level */
 
-//#define TCP_NODELAY	???
+enum{
+	 _CTRU_TCP_OPT = 0x2000,            /* Flag for tcp opt values */
+	 TCP_NODELAY   = 1 | _CTRU_TCP_OPT, /* Don't delay send to coalesce packets  */
+	 TCP_MAXSEG    = 2 | _CTRU_TCP_OPT,
+};
