e87ad0ad6c0dcfb52504da5a486c708d1c3bf1d8
yellows8 <yellows8@users.noreply.github.com>
2015-11-08 21:28:39 -0500

    Implemented code for getting the system-version from NAND NVer/CVer.

--- libctru/source/os.c
+++ libctru/source/os.c
@@ -1,10 +1,16 @@
 #include <3ds/types.h>
 #include <3ds/os.h>
 #include <3ds/svc.h>
+#include <3ds/romfs.h>
 #include <3ds/services/ptm.h>
+#include <3ds/services/fs.h>
+#include <3ds/services/cfgu.h>
 
 #include <sys/time.h>
 #include <reent.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
 
 #define TICKS_PER_USEC 268.123480
 #define TICKS_PER_MSEC 268123.480
@@ -28,6 +34,26 @@ static volatile datetime_t* __datetime1 =
 	(datetime_t*) 0x1FF81040;
 
 __attribute__((weak)) bool __ctru_speedup = false;
+
+static u32 __NVer_tidlow_regionarray[7] = {//See here regarding regions: http://3dbrew.org/wiki/Nandrw/sys/SecureInfo_A
+0x00016202, //JPN
+0x00016302, //USA
+0x00016102, //EUR
+0x00016202, //"AUS"
+0x00016402, //CHN
+0x00016502, //KOR
+0x00016602, //TWN
+};
+
+static u32 __CVer_tidlow_regionarray[7] = {
+0x00017202, //JPN
+0x00017302, //USA
+0x00017102, //EUR
+0x00017202, //"AUS"
+0x00017402, //CHN
+0x00017502, //KOR
+0x00017602 //TWN
+};
 
 //---------------------------------------------------------------------------------
 u32 osConvertVirtToPhys(u32 vaddr) {
@@ -172,3 +198,92 @@ void osSetSpeedupEnable(bool enable)
 	__ctru_speedup = enable;
 	__ctru_speedup_config();
 }
+
+static Result __read_versionbin(FS_archive archive, FS_path fileLowPath, OS_VersionBin *versionbin)
+{
+	Result ret = 0;
+	Handle filehandle = 0;
+	FILE *f = NULL;
+
+	ret = FSUSER_OpenFileDirectly(NULL, &filehandle, archive, fileLowPath, FS_OPEN_READ, 0x0);
+	if(ret<0)return ret;
+
+	ret = romfsInitFromFile(filehandle, 0x0);
+	if(ret<0)return ret;
+
+	f = fopen("romfs:/version.bin", "r");
+	if(f==NULL)
+	{
+		ret = errno;
+	}
+	else
+	{
+		if(fread(versionbin, 1, sizeof(OS_VersionBin), f) != sizeof(OS_VersionBin))ret = -10;
+		fclose(f);
+	}
+
+	romfsExit();
+
+	return ret;
+}
+
+Result osGetSystemVersionData(OS_VersionBin *nver_versionbin, OS_VersionBin *cver_versionbin)
+{
+	Result ret=0;
+	u8 region=0;
+
+	u32 archive_lowpath_data[0x10>>2];
+	u32 file_lowpath_data[0x14>>2];
+
+	FS_archive archive;
+	FS_path fileLowPath;
+
+	memset(archive_lowpath_data, 0, sizeof(file_lowpath_data));
+	memset(file_lowpath_data, 0, sizeof(file_lowpath_data));
+
+	archive.id = 0x2345678a;
+	archive.lowPath.type = PATH_BINARY;
+	archive.lowPath.size = 0x10;
+	archive.lowPath.data = (u8*)archive_lowpath_data;
+
+	fileLowPath.type = PATH_BINARY;
+	fileLowPath.size = 0x14;
+	fileLowPath.data = (u8*)file_lowpath_data;
+
+	archive_lowpath_data[1] = 0x000400DB;
+
+	ret = initCfgu();
+	if(ret<0)return ret;
+
+	ret = CFGU_SecureInfoGetRegion(&region);
+	if(ret<0)return ret;
+
+	if(region>=7)return -9;
+
+	exitCfgu();
+
+	archive_lowpath_data[0] = __NVer_tidlow_regionarray[region];
+	ret = __read_versionbin(archive, fileLowPath, nver_versionbin);
+	if(ret<0)return ret;
+
+	archive_lowpath_data[0] = __CVer_tidlow_regionarray[region];
+	ret = __read_versionbin(archive, fileLowPath, cver_versionbin);
+	return ret;
+}
+
+Result osGetSystemVersionDataString(OS_VersionBin *nver_versionbin, OS_VersionBin *cver_versionbin, char *sysverstr, u32 sysverstr_maxsize)
+{
+	Result ret=0;
+	OS_VersionBin nver_versionbin_tmp, cver_versionbin_tmp;
+
+	if(nver_versionbin==NULL)nver_versionbin = &nver_versionbin_tmp;
+	if(cver_versionbin==NULL)cver_versionbin = &cver_versionbin_tmp;
+
+	ret = osGetSystemVersionData(nver_versionbin, cver_versionbin);
+	if(ret!=0)return ret;
+
+	snprintf(sysverstr, sysverstr_maxsize, "%u.%u.%u-%u%c\n", cver_versionbin->mainver, cver_versionbin->minor, cver_versionbin->build, nver_versionbin->mainver, nver_versionbin->region);
+
+	return 0;
+}
+
