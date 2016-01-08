eba67d69cacfe00796f7cf68241c5c32d5efeb9f
Dave Murphy <davem@devkitpro.org>
2016-01-08 19:07:09 +0000

    add macros for color codes

--- libctru/include/3ds/console.h
+++ libctru/include/3ds/console.h
@@ -23,6 +23,17 @@
 extern "C" {
 #endif
 
+#define CONSOLE_ESC(x) "\x1b[" #x
+#define CONSOLE_RESET   CONSOLE_ESC(0m)
+#define CONSOLE_BLACK   CONSOLE_ESC(30m)
+#define CONSOLE_RED     CONSOLE_ESC(31;1m)
+#define CONSOLE_GREEN   CONSOLE_ESC(32;1m)
+#define CONSOLE_YELLOW  CONSOLE_ESC(33;1m)
+#define CONSOLE_BLUE    CONSOLE_ESC(34;1m)
+#define CONSOLE_MAGENTA CONSOLE_ESC(35;1m)
+#define CONSOLE_CYAN    CONSOLE_ESC(36;1m)
+#define CONSOLE_WHITE   CONSOLE_ESC(37;1m)
+
 /// A callback for printing a character.
 typedef bool(*ConsolePrint)(void* con, int c);
 
