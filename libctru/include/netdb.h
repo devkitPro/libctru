517b1c9503c88ee2c2ba834f1dbceb205da35dc7
Lectem <lectem@gmail.com>
2016-01-13 12:18:16 -0500

    added SOC:u getnameinfo

--- libctru/include/netdb.h
+++ libctru/include/netdb.h
@@ -17,6 +17,21 @@ struct hostent {
 	char	*h_addr;
 };
 
+#define NI_MAXHOST     1025
+#define NI_MAXSERV       32
+
+#define NI_NOFQDN      0x01
+#define NI_NUMERICHOST 0x02
+#define NI_NAMEREQD    0x04
+#define NI_NUMERICSERV 0x00 /* probably 0x08 but services names are never resolved */
+#define NI_DGRAM       0x00 /* probably 0x10 but services names are never resolved */
+
+#define EAI_FAMILY   (-303)
+#define EAI_MEMORY   (-304)
+#define EAI_NONAME   (-305)
+#define EAI_SOCKTYPE (-307)
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -26,6 +41,10 @@ extern "C" {
 	struct hostent*	gethostbyaddr(const void *addr, socklen_t len, int type);
 	void		herror(const char *s);
 	const char*	hstrerror(int err);
+
+	int getnameinfo(const struct sockaddr *sa, socklen_t salen,
+		char *host, socklen_t hostlen,
+		char *serv, socklen_t servlen, int flags);
 
 #ifdef __cplusplus
 }
