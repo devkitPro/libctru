464d6c636321f8d3f6b20d368a8ff3e749e7c9fb
Lectem <lectem@gmail.com>
2016-01-16 21:13:22 -0500

    SOC:u added close/shutdown sockets

---
+++ libctru/source/services/soc/soc_shutdownsockets.c
@@ -0,0 +1,14 @@
+#include "soc_common.h"
+#include <3ds/ipc.h>
+#include <3ds/result.h>
+
+int SOCU_ShutdownSockets()
+{
+	u32 *cmdbuf = getThreadCommandBuffer();
+
+	cmdbuf[0] = IPC_MakeHeader(0x19,0,0); // 0x190000
+
+	int ret = svcSendSyncRequest(SOCU_handle);
+	if(R_FAILED(ret))return ret;
+	return cmdbuf[1];
+}
