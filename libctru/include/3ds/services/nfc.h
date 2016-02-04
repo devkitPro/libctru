4ebf6c378136d3c2ba2032c3f9272e5512ed0f49
Javi <javimad@yandex.com>
2016-02-04 17:18:26 +0100

    Fixed prefix and parameters declaration

--- libctru/include/3ds/services/nfc.h
+++ libctru/include/3ds/services/nfc.h
@@ -24,44 +24,44 @@ Handle nfcGetSessionHandle(void);
  * @brief Initialize NFC module.
  * @param type Unknown, can be either value 0x1 or 0x2.
  */
-Result nfc_Initialize(u8 type);
+Result NFC_Initialize(u8 type);
 
 /**
  * @brief Shutdown NFC module.
  * @param type Unknown.
  */
-Result nfc_Shutdown(u8 type);
+Result NFC_Shutdown(u8 type);
 
 /**
  * @brief O3DS starts communication with the O3DS NFC hardware. N3DS just checks state for this command.
  */
-Result nfc_StartCommunication();
+Result NFC_StartCommunication(void);
 
 /**
  * @brief O3DS stops communication with the O3DS NFC hardware. N3DS just uses code used internally by NFC:StopTagScanning for this.
  */
-Result nfc_StopCommunication();
+Result NFC_StopCommunication(void);
 
 /**
  * @brief Starts scanning for NFC tags.
  * @param unknown Unknown.
  */
-Result nfc_StartTagScanning(u16 unknown);
+Result NFC_StartTagScanning(u16 unknown);
 
 /**
  * @brief Stops scanning for NFC tags.
  */
-Result nfc_StopTagScanning();
+Result NFC_StopTagScanning(void);
 
 /**
  * @brief Read Amiibo NFC data and load in memory.
  */
-Result nfc_LoadAmiiboData();
+Result NFC_LoadAmiiboData(void);
 
 /**
  * @brief If the tagstate is valid, it then sets the current tagstate to 3.
  */
-Result nfc_ResetTagScanState();
+Result NFC_ResetTagScanState(void);
 
 /**
  * @brief Returns the current NFC tag state.
@@ -75,5 +75,5 @@ Result nfc_ResetTagScanState();
  * - 4: NFC tag is now out of range, where the NFC tag was previously in range. This occurs automatically without using any NFC service commands. Once this state is entered, it won't automatically change to anything else when the tag is moved in range again. Hence, if you want to keep doing tag scanning after this, you must stop+start scanning.
  * - 5: NFC tag data was successfully loaded. This is set by NFC:LoadAmiiboData when successful. 
  */
-Result nfc_GetTagState(u8 *state);
+Result NFC_GetTagState(u8 *state);
 
