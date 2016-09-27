0eb70a435063090d614b4caef4cfb6eadd4f1d56
TuxSH <tuxsh@sfr.fr>
2016-09-27 23:16:11 +0200

    Implement NS_LaunchFIRM and NS_LaunchApplicationFIRM (#316)

--- libctru/include/3ds/services/ns.h
+++ libctru/include/3ds/services/ns.h
@@ -11,12 +11,25 @@ Result nsInit(void);
 void nsExit(void);
 
 /**
+ * @brief Launches a title and the required firmware (only if necessary).
+ * @param titleid ID of the title to launch, 0 for gamecard, JPN System Settings' titleID for System Settings.
+ */
+Result NS_LaunchFIRM(u64 titleid);
+
+/**
  * @brief Launches a title.
  * @param titleid ID of the title to launch, or 0 for gamecard.
  * @param launch_flags Flags used when launching the title.
  * @param procid Pointer to write the process ID of the launched title to.
  */
 Result NS_LaunchTitle(u64 titleid, u32 launch_flags, u32 *procid);
+
+/**
+ * @brief Launches a title and the required firmware.
+ * @param titleid ID of the title to launch, 0 for gamecard.
+ * @param flags Flags for firm-launch. bit0: require an application title-info structure in FIRM paramters to be specified via FIRM parameters. bit1: if clear, NS will check certain Configuration Memory fields.
+ */
+Result NS_LaunchApplicationFIRM(u64 titleid, u32 flags);
 
 /**
  * @brief Reboots to a title.
