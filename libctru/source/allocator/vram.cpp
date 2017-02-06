eb9a98bcf968b1500ca5ceee4a18b8c57741d176
DiscostewSM <discostewsm@gmail.com>
2017-02-06 11:52:57 -0800

    Assignable VRAM bank allocations
    This change integrates being able to allocate to a specific VRAM bank
    into the existing implementation.

--- libctru/source/allocator/vram.cpp
+++ libctru/source/allocator/vram.cpp
@@ -8,21 +8,28 @@ extern "C"
 #include "mem_pool.h"
 #include "addrmap.h"
 
-static MemPool sVramPool;
+static MemPool sVramPoolA;
+static MemPool sVramPoolB;
 
 static bool vramInit()
 {
-	auto blk = MemBlock::Create((u8*)0x1F000000, 0x00600000);
-	if (blk)
+	auto blkA = MemBlock::Create((u8*)0x1F000000, 0x00300000);
+	if (blkA)
 	{
-		sVramPool.AddBlock(blk);
-		rbtree_init(&sAddrMap, addrMapNodeComparator);
-		return true;
+		auto blkB = MemBlock::Create((u8*)0x1F300000, 0x00300000);
+		if(blkB)
+		{
+			sVramPoolA.AddBlock(blkA);
+			sVramPoolB.Addblock(blkB);
+			rbtree_init(&sAddrMap, addrMapNodeComparator);
+			return true;
+		}
+		free(blkA);
 	}
 	return false;
 }
 
-void* vramMemAlign(size_t size, size_t alignment)
+void* vramBankMemAlign(VRAM_ALLOCATOR bank, size_t size, size_t alignment)
 {
 	// Enforce minimum alignment
 	if (alignment < 16)
@@ -39,27 +46,65 @@ void* vramMemAlign(size_t size, size_t alignment)
 		return nullptr;
 
 	// Initialize the pool if it is not ready
-	if (!sVramPool.Ready() && !vramInit())
+	if (!sVramPoolA.Ready() && !sVramPoolB.Ready() && !vramInit())
 		return nullptr;
 
 	// Allocate the chunk
 	MemChunk chunk;
-	if (!sVramPool.Allocate(chunk, size, shift))
-		return nullptr;
+	int bankSet;
+	if(bank != VRAM_B)
+	{
+		// Attempt Bank A (for both cases)
+		if(!sVramPoolA.Allocate(chunk, size, shift))
+		{
+			if(bank == VRAM_A)
+				return nullptr;
+			else
+			{
+				// Attempt Bank B
+				if(!sVramPoolB.Allocate(chunk, size, shift))
+					return nullptr;
+				bankSet = 1;
+			}
+		}
+		else
+			bankset = 0;
+	}
+	else
+	{
+		// Attempt Bank B only
+		if(!sVramPoolB.Allocate(chunk, size, shift))
+			return nullptr;
+		bankSet = 1;
+	}
 
 	auto node = newNode(chunk);
 	if (!node)
 	{
-		sVramPool.Deallocate(chunk);
+		if(bankSet)
+			sVramPoolB.Deallocate(chunk);
+		else
+			sVramPoolA.Deallocate(chunk);
 		return nullptr;
 	}
+
 	if (rbtree_insert(&sAddrMap, &node->node));
 	return chunk.addr;
 }
 
+void* vramMemAlign(size_t size, size_t alignment)
+{
+	return vramBankMemAlign(VRMA_AB, size, alignment);
+}
+
+void* vramBankAlloc(VRAM_ALLOCATOR bank, size_t size)
+{
+	return vramBankMemAlign(bank, size, 0x80);
+}
+
 void* vramAlloc(size_t size)
 {
-	return vramMemAlign(size, 0x80);
+	return vramBankMemAlign(VRAM_AB, size, 0x80);
 }
 
 void* vramRealloc(void* mem, size_t size)
@@ -74,13 +119,30 @@ void vramFree(void* mem)
 	if (!node) return;
 
 	// Free the chunk
-	sVramPool.Deallocate(node->chunk);
+	if((u32)mem < 0x1F300000)
+		sVramPoolA.Deallocate(node->chunk);
+	else
+		sVramPoolB.Deallocate(node->chunk);
 
 	// Free the node
 	delNode(node);
 }
 
+u32 vramBankSpaceFree(VRAM_ALLOCATOR bank);
+{
+	if(bank != VRAM_B)
+	{
+		u32 space = sVramPoolA.GetFreeSpace();
+		if(bank == VRAM_A)
+			return space;
+		else
+			return space + sVramPoolB.GetFreeSpace();
+	}
+	else
+		return sVramPoolB.GetFreeSpace();
+}
+
 u32 vramSpaceFree()
 {
-	return sVramPool.GetFreeSpace();
+	return sVramPoolA.GetFreeSpace() + sVramPoolB.GetFreeSpace();
 }
