d4812f0118b998de0cb7a9709be9a4eb65bd84d0
Kartik <pokemonemerald0@gmail.com>
2017-03-02 13:16:12 +0530

    Fix according to review

--- libctru/include/3ds/applets/error.h
+++ libctru/include/3ds/applets/error.h
@@ -6,8 +6,8 @@
 
  enum
 {
-    ERROR_LANGUAGE_FLAG = 0x100,    ///<??-Unknown flag
-    ERROR_WORD_WRAP_FLAG = 0x200    ///<??-Unknown flag
+	ERROR_LANGUAGE_FLAG = 0x100,    ///<??-Unknown flag
+	ERROR_WORD_WRAP_FLAG = 0x200    ///<??-Unknown flag
 };
 
 /*
@@ -15,26 +15,26 @@ Type of Error applet to be called
 */
 typedef enum 
 {
-    ERROR_CODE = 0,                                             ///< Displays the infrastructure communications-related error message corresponding to the error code.
-    ERROR_TEXT,                                                 ///< Displays text passed to this applet.
-    EULA,                                                    	///< Displays the EULA
-    ERROR_TYPE_EULA_FIRST_BOOT,                                 ///< Use prohibited.
-    ERROR_TYPE_EULA_DRAW_ONLY, 	                                ///< Use prohibited.
-    ERROR_TYPE_AGREE,                                           ///< Use prohibited.
-    ERROR_CODE_LANGUAGE = ERROR_CODE | ERROR_LANGUAGE_FLAG ,    ///< Displays a network error message in a specified language.
-    ERROR_TEXT_LANGUAGE = ERROR_TEXT | ERROR_LANGUAGE_FLAG,     ///< Displays text passed to this applet in a specified language.
-    EULA_LANGUAGE = EULA | ERROR_LANGUAGE_FLAG,                 ///< Displays EULA in a specified language.
-    ERROR_TEXT_WORD_WRAP = ERROR_TEXT | ERROR_WORD_WRAP_FLAG,   ///< Displays the custom error message passed to this applet with automatic line wrapping
-    ERROR_TEXT_LANGUAGE_WORD_WRAP = ERROR_TEXT | ERROR_LANGUAGE_FLAG | ERROR_WORD_WRAP_FLAG,  ///< Displays the custom error message with automatic line wrapping and in the specified language.
+	ERROR_CODE = 0,                                             ///< Displays the infrastructure communications-related error message corresponding to the error code.
+	ERROR_TEXT,                                                 ///< Displays text passed to this applet.
+	EULA,                                                       ///< Displays the EULA
+	ERROR_TYPE_EULA_FIRST_BOOT,                                 ///< Use prohibited.
+	ERROR_TYPE_EULA_DRAW_ONLY,                                  ///< Use prohibited.
+	ERROR_TYPE_AGREE,                                           ///< Use prohibited.
+	ERROR_CODE_LANGUAGE = ERROR_CODE | ERROR_LANGUAGE_FLAG,     ///< Displays a network error message in a specified language.
+	ERROR_TEXT_LANGUAGE = ERROR_TEXT | ERROR_LANGUAGE_FLAG,     ///< Displays text passed to this applet in a specified language.
+	EULA_LANGUAGE = EULA | ERROR_LANGUAGE_FLAG,                 ///< Displays EULA in a specified language.
+	ERROR_TEXT_WORD_WRAP = ERROR_TEXT | ERROR_WORD_WRAP_FLAG,   ///< Displays the custom error message passed to this applet with automatic line wrapping
+	ERROR_TEXT_LANGUAGE_WORD_WRAP = ERROR_TEXT | ERROR_LANGUAGE_FLAG | ERROR_WORD_WRAP_FLAG,   ///< Displays the custom error message with automatic line wrapping and in the specified language.
 }ErrorType;
 
 
- ///< Flags for the Upper Screen.Does nothing even if specified.
+///< Flags for the Upper Screen.Does nothing even if specified.
 
 typedef enum
 {
-    NORMAL = 0,    
-    STEREO,        
+	NORMAL = 0,    
+	STEREO,        
 }screen_flag;
 
 
@@ -42,20 +42,20 @@ typedef enum
 
 typedef enum
 {
-    DEFAULT = 0,   
-    JAPANESE,      
-    ENGLISH,       
-    FRENCH,        
-    GERMAN,        
-    ITALIAN,       
-    SPANISH,       
-    SIMP_CHINESE,  
-    KOREAN,        
-    DUTCH,         
-    PORTUGUESE,    
-    RUSSIAN,       
-    TRAD_CHINESE,  
-   
+	DEFAULT = 0,   
+	JAPANESE,      
+	ENGLISH,       
+	FRENCH,        
+	GERMAN,        
+	ITALIAN,       
+	SPANISH,       
+	SIMP_CHINESE,  
+	KOREAN,        
+	DUTCH,         
+	PORTUGUESE,    
+	RUSSIAN,       
+	TRAD_CHINESE,  
+	
 }err_lang;
 
 
@@ -63,39 +63,29 @@ typedef enum
 
 typedef enum 
 {
-    UNKNOWN = -1,       
-    NONE    = 0,        
-    SUCCESS,            
-    NOT_SUPPORTED,      
-    HOME_BUTTON = 10,   
-    SOFTWARE_RESET,     
-    POWER_BUTTON,       
+	UNKNOWN = -1,       
+	NONE    = 0,        
+	SUCCESS,            
+	NOT_SUPPORTED,      
+	HOME_BUTTON = 10,   
+	SOFTWARE_RESET,     
+	POWER_BUTTON,       
 }ReturnCode;
 
 ///< Structure to be passed to the applet.Shouldn't be modified directly.
 
 typedef struct 
 {
-    ErrorType   errorType;
-
-    int         errorCode;
-
-    screen_flag upperScreenFlag;
-
-    err_lang    useLanguage;
-
-    u16         Text[1900];
-
-    bool        homeButton;
-
-    bool        softwareReset;
-
-    bool        appJump;
-
-    ReturnCode  returnCode;
-
-    u16         eulaVersion;
-
+	ErrorType errorType;
+	int errorCode;
+	screen_flag upperScreenFlag;
+	err_lang useLanguage;
+	u16 Text[1900];
+	bool homeButton;
+	bool softwareReset;
+	bool appJump;
+	ReturnCode returnCode;
+	u16 eulaVersion;
 }ErrConf;
 /**
 * @brief Init the error applet.
