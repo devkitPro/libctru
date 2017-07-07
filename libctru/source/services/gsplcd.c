3cea84f3153b92b5f617ff80e82da199f28ac274
Dave Murphy <davem@devkitpro.org>
2017-07-07 16:09:31 +0100

    fix whitespace

--- libctru/source/services/gsplcd.c
+++ libctru/source/services/gsplcd.c
@@ -68,15 +68,16 @@ Result GSPLCD_GetVendors(u8 *vendors)
 
 Result GSPLCD_GetBrightness(u32 screen, u32 *brightness)
 {
-    u32 *cmdbuf = getThreadCommandBuffer();
- 
-    cmdbuf[0] = IPC_MakeHeader(0x15,1,0); // 0x150040
-    cmdbuf[1] = screen;
- 
-    Result ret = 0;
-    if (R_FAILED(ret = svcSendSyncRequest(gspLcdHandle))) return ret;
-   
-    *brightness = cmdbuf[2];
-   
-    return cmdbuf[2];
+	u32 *cmdbuf = getThreadCommandBuffer();
+	
+	cmdbuf[0] = IPC_MakeHeader(0x15,1,0); // 0x150040
+	cmdbuf[1] = screen;
+	
+	Result ret = 0;
+	if (R_FAILED(ret = svcSendSyncRequest(gspLcdHandle))) return ret;
+	
+	*brightness = cmdbuf[2];
+	
+	return cmdbuf[2];
 }
+
