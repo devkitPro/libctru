1a267d52fcd7e52eeb6fc31bae0a014077c66023
Pirater12 <pokemonemerald0@gmail.com>
2017-08-20 23:08:50 +0530

    Update

--- libctru/include/3ds/services/mcuHwc.h
+++ libctru/include/3ds/services/mcuHwc.h
@@ -1,11 +1,11 @@
 /**
- * @file mcuHWC.h
+ * @file mcuHwc.h
  * @brief mcuHWC service.
  */
 #pragma once
 
 /// Initializes mcuHWC.
-Result  mcuHWCInit(void);
+Result mcuHWCInit(void);
 
 /// Exits mcuHWC.
 void mcuHWCExit(void);
@@ -13,18 +13,18 @@ void mcuHWCExit(void);
 /**
  * @brief Reads data from a MCU Register
  * @param reg Register number. See https://www.3dbrew.org/wiki/I2C_Registers#Device_3 for more info
- * @param size Size of data to be read
  * @param data Pointer to write the data to.
+ * @param size Size of data to be read
  */
-Result mcuReadRegister(u8 reg, u32 size, u8* data);
+Result mcuReadRegister(u8 reg, void *data, u32 size);
 
 /**
  * @brief Writes data to a MCU Register
  * @param reg Register number. See https://www.3dbrew.org/wiki/I2C_Registers#Device_3 for more info
+ * @param data Pointer to write the data to.
  * @param size Size of data to be written
- * @param data Pointer to write the data from.
  */
-Result mcuWriteRegister(u8 reg, u32 size, u8* data);
+Result mcuWriteRegister(u8 reg, const void *data, u32 size);
 
 /**
  * @brief Gets the battery voltage
@@ -43,4 +43,3 @@ Result mcuGetBatteryLevel(u8 *level);
  * @param level Pointer to write the slider level to.
  */
 Result mcuGetSoundSliderLevel(u8 *level);
-
--- libctru/source/services/mcuHwc.c
+++ libctru/source/services/mcuHwc.c
@@ -1,5 +1,5 @@
 #include <3ds.h>
-#include <3ds/services/mcuHWC.h>
+#include <3ds/services/mcuHwc.h>
 
 static Handle mcuHWCHandle;
 static int mcuHWCRefCount;
@@ -18,35 +18,35 @@ void mcuHWCExit(void)
 	svcCloseHandle(mcuHWCHandle);
 }
 
-Result mcuReadRegister(u8 reg, u32 size, u8* data)
+Result mcuReadRegister(u8 reg, void* data, u32 size)
 {
 	Result ret = 0;
 	u32 *cmdbuf = getThreadCommandBuffer();
-	
+
 	cmdbuf[0] = IPC_MakeHeader(0x1,2,2); // 0x10082
 	cmdbuf[1] = reg;
 	cmdbuf[2] = size;
-	cmdbuf[3] = size << 4 | 0xC;
+	cmdbuf[3] = IPC_Desc_Buffer (size, IPC_BUFFER_W);
 	cmdbuf[4] = (u32)data;
-	
+
 	if(R_FAILED(ret = svcSendSyncRequest(mcuHWCHandle)))return ret;
-	
+
 	return (Result)cmdbuf[1];
 }
 
-Result mcuWriteRegister(u8 reg, u32 size, u8* data)
+Result mcuWriteRegister(u8 reg, const void *data, u32 size)
 {
 	Result ret = 0;
 	u32 *cmdbuf = getThreadCommandBuffer();
-	
+
 	cmdbuf[0] = IPC_MakeHeader(0x2,2,2); // 0x20082
 	cmdbuf[1] = reg;
 	cmdbuf[2] = size;
-	cmdbuf[3] = size << 4 | 0xA;
+	cmdbuf[3] = IPC_Desc_Buffer (size, IPC_BUFFER_R);
 	cmdbuf[4] = (u32)data;
-	
+
 	if(R_FAILED(ret = svcSendSyncRequest(mcuHWCHandle)))return ret;
-	
+
 	return (Result)cmdbuf[1];
 }
 
@@ -54,13 +54,13 @@ Result mcuGetBatteryVoltage(u8 *voltage)
 {
 	Result ret = 0;
 	u32 *cmdbuf = getThreadCommandBuffer();
-	
+
 	cmdbuf[0] = IPC_MakeHeader(0x4,0,0); // 0x40000
-	
+
 	if(R_FAILED(ret = svcSendSyncRequest(mcuHWCHandle)))return ret;
-	
-	*voltage = cmdbuf[2]; 
-	
+
+	*voltage = cmdbuf[2];
+
 	return (Result)cmdbuf[1];
 }
 
@@ -68,13 +68,13 @@ Result mcuGetBatteryLevel(u8 *level)
 {
 	Result ret = 0;
 	u32 *cmdbuf = getThreadCommandBuffer();
-	
+
 	cmdbuf[0] = IPC_MakeHeader(0x5,0,0); // 0x50000
-	
+
 	if(R_FAILED(ret = svcSendSyncRequest(mcuHWCHandle)))return ret;
-	
-	*level = cmdbuf[2]; 
-	
+
+	*level = cmdbuf[2];
+
 	return (Result)cmdbuf[1];
 }
 
@@ -82,12 +82,12 @@ Result mcuGetSoundSliderLevel(u8 *level)
 {
 	Result ret = 0;
 	u32 *cmdbuf = getThreadCommandBuffer();
-	
+
 	cmdbuf[0] = IPC_MakeHeader(0xB,0,0); // 0xB0000
-	
+
 	if(R_FAILED(ret = svcSendSyncRequest(mcuHWCHandle)))return ret;
-	
+
 	*level = cmdbuf[2];
-	
+
 	return (Result)cmdbuf[1];
 }
