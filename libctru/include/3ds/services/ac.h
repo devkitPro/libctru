6f64dee330e611ad699461eca43dc399bf96d5f9
Joel <sauskethunderkid@gmail.com>
2017-12-01 10:58:08 -0600

    Add a few more ACU functions (#379)

--- libctru/include/3ds/services/ac.h
+++ libctru/include/3ds/services/ac.h
@@ -18,3 +18,21 @@ Result acWaitInternetConnection(void);
  * @param out Pointer to output the current Wifi status to. (0 = not connected, 1 = O3DS Internet, 2 = N3DS Internet)
  */
 Result ACU_GetWifiStatus(u32 *out);
+
+/**
+ * @brief Gets the current Wifi status.
+ * @param out Pointer to output the current Wifi status to. (1 = not connected, 3 = connected)
+ */
+Result ACU_GetStatus(u32 *out);
+
+/**
+ * @brief Gets the current Wifi security mode.
+ * @param out Pointer to output the current Wifi security mode to. (0 = Open Authentication, 1 = WEP 40-bit, 2 = WEP 104-bit, 3 = WEP 128-bit, 4 = WPA TKIP, 5 = WPA2 TKIP, 6 = WPA AES, 7 = WPA2 AES)
+ */
+Result ACU_GetSecurityMode(u32 *out);
+
+/**
+ * @brief Gets the current Wifi SSID length.
+ * @param out Pointer to output the current Wifi SSID length to.
+ */
+Result ACU_GetSSIDLength(u32 *out);
