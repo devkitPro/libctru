fdf31f7556493335477d6157e90c48f94b549e0e
TuxSH <tuxsh@sfr.fr>
2018-01-07 01:00:19 +0100

    Multiple srv:pm bugfixes

--- libctru/include/3ds/services/srvpm.h
+++ libctru/include/3ds/services/srvpm.h
@@ -35,7 +35,7 @@ Result SRVPM_PublishToAll(u32 notificationId);
  * @param count Number of services within the service access control data.
  * @param serviceAccessControlList Service Access Control list.
  */
-Result SRVPM_RegisterProcess(u32 pid, u32 count, char (*serviceAccessControlList)[8]);
+Result SRVPM_RegisterProcess(u32 pid, u32 count, const char (*serviceAccessControlList)[8]);
 
 /**
  * @brief Unregisters a process with SRV.
