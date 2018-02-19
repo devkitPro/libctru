1b93b23feea51d07c877e123c75f8fe724b95583
Joel16 <sauskethunderkid@gmail.com>
2018-02-19 17:10:45 -0600

    Add ACU_Proxy functions and minor changes
    - More descriptive/accurate docs
    - Use enum for security mode.

--- libctru/include/3ds/services/ac.h
+++ libctru/include/3ds/services/ac.h
@@ -4,6 +4,18 @@
  */
 #pragma once
 
+/// WiFi security modes.
+typedef enum {
+    AC_OPEN = 0,       ///< Open authentication.
+    AC_WEP_40BIT = 1,  ///< WEP 40-bit authentication.
+    AC_WEP_104BIT = 2, ///< WEP 104-bit authentication.
+    AC_WEP_128BIT = 3, ///< WEP 128-bit authentication.
+    AC_WPA_TKIP = 4,   ///< WPA TKIP authentication.
+    AC_WPA2_TKIP = 5,  ///< WPA2 TKIP authentication.
+    AC_WPA_AES = 6,    ///< WPA AES authentication.
+    AC_WPA2_AES = 7,   ///< WPA2 AES authentication.
+} acSecurityMode;
+
 /// Initializes AC.
 Result acInit(void);
 
@@ -14,28 +26,58 @@ void acExit(void);
 Result acWaitInternetConnection(void);
 
 /**
- * @brief Gets the current Wifi status.
- * @param out Pointer to output the current Wifi status to. (0 = not connected, 1 = O3DS Internet, 2 = N3DS Internet)
+ * @brief Gets the connected WiFi status.
+ * @param out Pointer to output the connected WiFi status to. (0 = not connected, 1 = O3DS Internet, 2 = N3DS Internet)
  */
-Result ACU_GetWifiStatus(u32 *out);
+Result ACU_GetWiFiStatus(u32 *out);
 
 /**
- * @brief Gets the current Wifi status.
- * @param out Pointer to output the current Wifi status to. (1 = not connected, 3 = connected)
+ * @brief Gets the connected WiFi status.
+ * @param out Pointer to output the connected WiFi status to. (1 = not connected, 3 = connected)
  */
 Result ACU_GetStatus(u32 *out);
 
 /**
- * @brief Gets the current Wifi security mode.
- * @param out Pointer to output the current Wifi security mode to. (0 = Open Authentication, 1 = WEP 40-bit, 2 = WEP 104-bit, 3 = WEP 128-bit, 4 = WPA TKIP, 5 = WPA2 TKIP, 6 = WPA AES, 7 = WPA2 AES)
+ * @brief Gets the connected WiFi security mode.
+ * @param mode Pointer to output the connected WiFi security mode to. (0 = Open Authentication, 1 = WEP 40-bit, 2 = WEP 104-bit, 3 = WEP 128-bit, 4 = WPA TKIP, 5 = WPA2 TKIP, 6 = WPA AES, 7 = WPA2 AES)
  */
-Result ACU_GetSecurityMode(u32 *out);
+Result ACI_GetSecurityMode(acSecurityMode *mode);
 
 /**
- * @brief Gets the current Wifi SSID length.
- * @param out Pointer to output the current Wifi SSID length to.
+ * @brief Gets the connected WiFi SSID.
+ * @param SSID Pointer to output the connected WiFi SSID to.
+ */
+Result ACU_GetSSID(char *SSID);
+
+/**
+ * @brief Gets the connected WiFi SSID length.
+ * @param out Pointer to output the connected WiFi SSID length to.
  */
 Result ACU_GetSSIDLength(u32 *out);
+
+/**
+ * @brief Determines whether proxy is enabled for the connected network.
+ * @param enable Pointer to output the proxy status to.
+ */
+Result ACU_GetProxyEnable(bool *enable);
+
+/**
+ * @brief Gets the connected network's proxy port.
+ * @param out Pointer to output the proxy port to.
+ */
+Result ACU_GetProxyPort(u32 *out);
+
+/**
+ * @brief Gets the connected network's proxy username.
+ * @param username Pointer to output the proxy username to.
+ */
+Result ACU_GetProxyUserName(char *username);
+
+/**
+ * @brief Gets the connected network's proxy password.
+ * @param password Pointer to output the proxy password to.
+ */
+Result ACU_GetProxyPassword(char *password);
 
 /**
  * @brief Gets the last error to occur during a connection.
