ec73a97778f1d7276afa2d6375078b750e38cba8
ZeroSkill1 <timmskiller102@outlook.com>
2026-05-20 22:11:48 +0300

    mcu: fix enum typedefs

--- libctru/include/3ds/services/mcu_common.h
+++ libctru/include/3ds/services/mcu_common.h
@@ -8,7 +8,7 @@
 #include <3ds/types.h>
 
 /// SRV notifications published by the MCU system module.
-enum MCU_InterruptNotification
+typedef enum MCU_InterruptNotification
 {
 	MCUNOTIF_SHELL_STATE_CHANGE    = 0x200, ///< The shell state has changed.
 	MCUNOTIF_POWER_BUTTON_PRESSED  = 0x202, ///< The power button was pressed.
@@ -21,10 +21,10 @@ enum MCU_InterruptNotification
 	MCUNOTIF_AC_ADAPTER_CONNECTED  = 0x20E, ///< The AC adapter was connected.
 	MCUNOTIF_STARTED_CHARGING      = 0x20F, ///< The battery started charging.
 	MCUNOTIF_STOPPED_CHARGING      = 0x210, ///< The battery stopped charging.
-};
+} MCU_InterruptNotification;
 
 /// MCU interrupts.
-enum MCU_Interrupt
+typedef enum MCU_Interrupt
 {
 	MCUINT_POWER_BUTTON_PRESS          = BIT(0),  ///< The power button was pressed.
 	MCUINT_POWER_BUTTON_HELD           = BIT(1),  ///< The power button was held.
@@ -42,57 +42,57 @@ enum MCU_Interrupt
 	MCUINT_CRITICAL_BATTERY            = BIT(13), ///< The battery is critically low.
 	MCUINT_CHARGING_STOP               = BIT(14), ///< The battery stopped charging.
 	MCUINT_CHARGING_START              = BIT(15), ///< The battery started charging.
-	
+
 	MCUINT_VOL_SLIDER                  = BIT(22), ///< The position of the volume slider changed.
-	
+
 	MCUINT_LCD_OFF                     = BIT(24), ///< The LCDs turned off.
 	MCUINT_LCD_ON                      = BIT(25), ///< The LCDs turned on.
 	MCUINT_BOT_BACKLIGHT_OFF           = BIT(26), ///< The bottom screen backlight turned off.
 	MCUINT_BOT_BACKLIGHT_ON            = BIT(27), ///< The bottom screen backlight turned on.
 	MCUINT_TOP_BACKLIGHT_OFF           = BIT(28), ///< The top screen backlight turned off.
 	MCUINT_TOP_BACKLIGHT_ON            = BIT(29), ///< The top screen backlight turned on.
-};
+} MCU_Interrupt;
 
 /// MCU register IDs.
-enum MCU_RegisterId
+typedef enum MCU_RegisterId
 {
 	MCUREG_VERSION_HIGH                 = 0x0,  ///< Major version of the MCU firmware.
 	MCUREG_VERSION_LOW                  = 0x1,  ///< Minor version of the MCU firmware.
-	
+
 	MCUREG_RESET_EVENTS                 = 0x2,  ///< @ref MCU_ResetEventFlags
-	
+
 	MCUREG_VCOM_TOP                     = 0x3,  ///< Flicker/VCOM value for the top screen.
 	MCUREG_VCOM_BOTTOM                  = 0x4,  ///< Flicker/VCOM value for the bottom screen.
-	
-	MCUREG_FIRMWARE_UPLOAD_0            = 0x5,  ///< Firmware upload register. 
+
+	MCUREG_FIRMWARE_UPLOAD_0            = 0x5,  ///< Firmware upload register.
 	MCUREG_FIRMWARE_UPLOAD_1            = 0x6,  ///< Firmware upload register.
 	MCUREG_FIRMWARE_UPLOAD_2            = 0x7,  ///< Firmware upload register.
-	
+
 	MCUREG_3D_SLIDER_POSITION           = 0x8,  ///< Position of the 3D slider.
 	MCUREG_VOLUME_SLIDER_POSITION       = 0x9,  ///< Position of the volume slider.
-	
+
 	MCUREG_BATTERY_PCB_TEMPERATURE      = 0xA,  ///< Temperature of the battery, measured on a sensor on the PCB.
 	MCUREG_BATTERY_PERCENTAGE_INT       = 0xB,  ///< Integer part of the battery percentage.
 	MCUREG_BATTERY_PERCENTAGE_FRAC      = 0xC,  ///< Fractional part of the battery percentage.
 	MCUREG_BATTERY_VOLTAGE              = 0xD,  ///< Voltage of the battery, in units of 20 mV.
-	
+
 	MCUREG_POWER_STATUS                 = 0xF,  ///< @ref MCU_PowerStatusFlags
-	
+
 	MCUREG_LEGACY_VERSION_HIGH          = 0xF,  ///< (Old MCU_FIRM only) Major firmware version.
 	MCUREG_LEGACY_VERSION_LOW           = 0x10, ///< (Old MCU_FIRM only) Minor firmware version.
 	MCUREG_LEGACY_FIRM_UPLOAD           = 0x3B, ///< (Old MCU_FIRM only) Firmware upload register.
-	
+
 	MCUREG_RECEIVED_IRQS                = 0x10, ///< Bitmask of received IRQs. @ref MCU_Interrupt
 	MCUREG_IRQ_MASK                     = 0x18, ///< Bitmask of enabled IRQs.  @ref MCU_Interrupt
-	
+
 	MCUREG_PWR_CTL                      = 0x20, ///< @ref MCU_PowerTrigger
 	MCUREG_LCD_PWR_CTL                  = 0x22, ///< @ref MCU_PowerStatusFlags
 	MCUREG_MCU_RESET_CTL                = 0x23, ///< Writing 'r' to this register resets the MCU. Stubbed on retail.
 	MCUREG_FORCE_SHUTDOWN_DELAY         = 0x24, ///< The amount of time, in units of 0.125s, for which the power button needs to be held to trigger a hard shutdown.
-	
+
 	MCUREG_VOLUME_UNK_25                = 0x25, ///< Unknown register. Used in mcu::SND
 	MCUREG_UNK_26                       = 0x26, ///< Unknown register. Used in mcu::CDC
-	
+
 	MCUREG_LED_BRIGHTNESS_STATE         = 0x28, ///< Brightness of the status LEDs.
 	MCUREG_POWER_LED_STATE              = 0x29, ///< @ref MCU_PowerLedState
 	MCUREG_WLAN_LED_STATE               = 0x2A, ///< Controls the WiFi LED.
@@ -100,7 +100,7 @@ enum MCU_RegisterId
 	MCUREG_3D_LED_STATE                 = 0x2C, ///< Controls the 3D LED (on models that have one).
 	MCUREG_NOTIFICATION_LED_STATE       = 0x2D, ///< @ref MCU_InfoLedPattern
 	MCUREG_NOTIFICATION_LED_CYCLE_STATE = 0x2E, ///< Bit 0 is set if the info (notification) LED has started a new cycle of its pattern.
-	
+
 	MCUREG_RTC_TIME_SECOND              = 0x30, ///< RTC second.
 	MCUREG_RTC_TIME_MINUTE              = 0x31, ///< RTC minute.
 	MCUREG_RTC_TIME_HOUR                = 0x32, ///< RTC hour.
@@ -109,16 +109,16 @@ enum MCU_RegisterId
 	MCUREG_RTC_TIME_MONTH               = 0x35, ///< RTC month.
 	MCUREG_RTC_TIME_YEAR                = 0x36, ///< RTC year.
 	MCUREG_RTC_TIME_CORRECTION          = 0x37, ///< RTC subsecond (RSUBC) correction value.
-	
+
 	MCUREG_RTC_ALARM_MINUTE             = 0x38, ///< RTC alarm minute.
 	MCUREG_RTC_ALARM_HOUR               = 0x39, ///< RTC alarm hour.
 	MCUREG_RTC_ALARM_DAY                = 0x3A, ///< RTC alarm day.
 	MCUREG_RTC_ALARM_MONTH              = 0x3B, ///< RTC alarm month.
 	MCUREG_RTC_ALARM_YEAR               = 0x3C, ///< RTC alarm year.
-	
+
 	MCUREG_TICK_COUNTER_LSB             = 0x3D, ///< MCU tick counter value (low byte).
 	MCUREG_TICK_COUNTER_MSB             = 0x3E, ///< MCU tick counter value (high byte).
-	
+
 	MCUREG_SENSOR_CONFIG                = 0x40, ///< @ref MCU_SensorConfig
 
 	MCUREG_ACCELEROMETER_MANUAL_REGID_R = 0x41, ///< Hardware register ID for use in manual accelerometer I²C reads.
@@ -138,23 +138,23 @@ enum MCU_RegisterId
 	MCUREG_PEDOMETER_STEP_DATA          = 0x4F, ///< @ref MCU_PedometerStepData
 	MCUREG_PEDOMETER_WRAP_MINUTE        = 0x50, ///< The minute within each RTC hour at which the step history should roll into the next hour.
 	MCUREG_PEDOMETER_WRAP_SECOND        = 0x51, ///< The second within each RTC hour at which the step history should roll into the next hour.
-	
+
 	MCUREG_VOLUME_CALIBRATION_MIN       = 0x58, ///< Lower bound of sound volume.
 	MCUREG_VOLUME_CALIBRATION_MAX       = 0x59, ///< Upper bound of sound volume.
-	
+
 	MCUREG_STORAGE_AREA_OFFSET          = 0x60, ///< Offset within the storage area to read/write to.
 	MCUREG_STORAGE_AREA                 = 0x61, ///< Input/output byte for write/read operations in the storage area.
-	
-	MCUREG_INFO                         = 0x7F, ///< System information register.
-};
 
-enum MCU_ResetEventFlags
+	MCUREG_INFO                         = 0x7F, ///< System information register.
+} MCU_RegisterId;
+
+typedef enum MCU_ResetEventFlags
 {
 	MCU_RESETFLG_RTC_TIME_LOST  = BIT(0), ///< RTC time was lost (as is the case when the battery is removed).
 	MCU_RESETFLG_WATCHDOG_RESET = BIT(1), ///< MCU Watchdog reset occurred.
-};
+} MCU_ResetEventFlags;
 
-enum MCU_PowerStatusFlags
+typedef enum MCU_PowerStatusFlags
 {
 	MCU_PWRSTAT_SHELL_OPEN        = BIT(1), ///< Set if the shell is open.
 	MCU_PWRSTAT_ADAPTER_CONNECTED = BIT(3), ///< Set if the AC adapter is connected.
@@ -162,15 +162,15 @@ enum MCU_PowerStatusFlags
 	MCU_PWRSTAT_BOTTOM_BL_ON      = BIT(5), ///< Set if the bottom backlight is on.
 	MCU_PWRSTAT_TOP_BL_ON         = BIT(6), ///< Set if the top backlight is on.
 	MCU_PWRSTAT_LCD_ON            = BIT(7)  ///< Set if the LCDs are on.
-};
+} MCU_PowerStatusFlags;
 
-enum MCU_PedometerControl
+typedef enum MCU_PedometerControl
 {
 	MCU_PEDOMETER_CLEAR      = BIT(0), ///< Clear the step history.
 	MCU_PEDOMETER_STEPS_FULL = BIT(4)  ///< Set when the step history is full.
-};
+} MCU_PedometerControl;
 
-enum MCU_PowerTrigger
+typedef enum MCU_PowerTrigger
 {
 	MCU_PWR_SHUTDOWN     = BIT(0), ///< Turn off the system.
 	MCU_PWR_RESET        = BIT(1), ///< Reset the MCU.
@@ -181,7 +181,7 @@ enum MCU_PowerTrigger
 	OLDMCU_BL_ON         = BIT(5), ///< (Old MCU_FIRM only) turn the backlights on.
 	OLDMCU_LCD_OFF       = BIT(6), ///< (Old MCU_FIRM only) turn the LCDs off.
 	OLDMCU_LCD_ON        = BIT(7), ///< (Old MCU_FIRM only) turn the LCDs on.
-};
+} MCU_PowerTrigger;
 
 typedef enum MCU_PowerLedState
 {
